a6e63c7077c17cf899dc02b59924a54fe74f787a
Mingzhao91 <edwardwhite7@hotmail.com>
2022-02-15 21:55:35 +0000

    routes for create and remove category

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -11,4 +11,38 @@ router.get("/", async (req, res) => {
   res.send(categoryList);
 });
 
+router.post("/", async (req, res) => {
+  let category = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+
+  category = await category.save();
+
+  if (!category) {
+    return res.status(404).send("Category cannot be created!");
+  }
+
+  res.send(category);
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const category = await Category.findByIdAndRemove(req.params.id);
+
+    if (category) {
+      return res
+        .status(200)
+        .json({ success: true, message: "Category is deleted." });
+    } else {
+      res
+        .status(404)
+        .json({ success: false, message: "Category is not found." });
+    }
+  } catch (error) {
+    return res.status(400).json({ success: false, error });
+  }
+});
+
 module.exports = router;
