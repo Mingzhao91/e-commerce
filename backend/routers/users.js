e892f23a009671e92e3042949507694ea407bfed
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-01 20:36:19 +0000

    post route to create a user

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -24,4 +24,27 @@ router.get("/:id", async (req, res) => {
   res.status(200).send(user);
 });
 
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    street: req.body.street,
+    city: req.body.city,
+    country: req.body.country,
+  });
+
+  user = await user.save();
+
+  if (!user) {
+    res.status(400).send("User cannot be created!");
+  }
+
+  res.status(200).send(user);
+});
+
 module.exports = router;
