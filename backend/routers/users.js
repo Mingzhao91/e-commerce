ea8d6a7a9c9b3904f26fd09e26c9f8ee75d535f2
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-01 20:35:30 +0000

    create get user  route

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,14 +1,27 @@
 const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
-router.get(``, async (req, res) => {
-  const userList = await User.find();
+router.get(`/`, async (req, res) => {
+  const userList = await User.find().select("-passwordHash");
 
   if (!userList) {
     res.status(500).json({ success: false });
   }
   res.send(userList);
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("-passwordHash");
+
+  if (!user) {
+    res
+      .status(500)
+      .json({ message: "Category with the given ID was not found." });
+  }
+
+  res.status(200).send(user);
 });
 
 module.exports = router;
