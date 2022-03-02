3b1b814460343b75ded87b3fdacbb0ee04f2c6ba
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-02 22:06:12 +0000

    add route for create an order

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -1,4 +1,5 @@
 const { Order } = require("../models/order");
+const { OrderItem } = require("../models/order-item");
 const express = require("express");
 const router = express.Router();
 
@@ -9,6 +10,42 @@ router.get(`/`, async (req, res) => {
     res.status(500).json({ success: false });
   }
   res.send(orderList);
+});
+
+router.post("/", async (req, res) => {
+  const orderItemsIds = await Promise.all(
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+      });
+
+      newOrderItem = await newOrderItem.save();
+
+      return newOrderItem._id;
+    })
+  );
+
+  let order = new Order({
+    orderItems: orderItemsIds,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: req.body.totalPrice,
+    user: req.body.user,
+  });
+
+  order = await order.save();
+
+  if (!order) {
+    res.status(400).send("Order cannot be created!");
+  }
+
+  res.status(200).send(order);
 });
 
 module.exports = router;
