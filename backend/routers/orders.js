ded70ac976ffcba304781402bbe11d43029ae6db
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-03 18:33:46 +0000

    create routes for update and delete order

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -80,4 +80,48 @@ router.post("/", async (req, res) => {
   res.status(200).send(order);
 });
 
+router.put("/:id", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid Order Id");
+  }
+
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!order) {
+    res.status(400).send("Order cannot be updated!");
+  }
+
+  res.status(200).send(order);
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      res.status(400).send("Invalid Order Id");
+    }
+
+    const order = await Order.findByIdAndRemove(req.params.id);
+
+    if (order) {
+      await order.orderItems.map(async (orderItemId) => {
+        await OrderItem.findByIdAndRemove(orderItemId);
+      });
+
+      res.status(200).json({ success: true, message: "Order is deleted." });
+    } else {
+      res.status(404).json({ success: false, message: "Order is not found." });
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
+});
+
 module.exports = router;
