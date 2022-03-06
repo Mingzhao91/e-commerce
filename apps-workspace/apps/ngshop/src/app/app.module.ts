30b2db9f685d5fb502d1ef579dcea085b46d469b
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-06 21:03:39 +0000

    initialise header and footer component

--- apps-workspace/apps/ngshop/src/app/app.module.ts
+++ apps-workspace/apps/ngshop/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppRoutingModule } from './app.routing';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductLstComponent } from './pages/product-lst/product-lst.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductLstComponent],
+    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductLstComponent, HeaderComponent, FooterComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent]
