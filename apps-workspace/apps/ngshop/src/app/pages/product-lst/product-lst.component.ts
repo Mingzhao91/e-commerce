db5af877a240887d67d68ebdc85f3e56b8e8a79a
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-06 21:11:34 +0000

    change component and directive prefix to ngshop

--- apps-workspace/apps/ngshop/src/app/pages/product-lst/product-lst.component.ts
+++ apps-workspace/apps/ngshop/src/app/pages/product-lst/product-lst.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'apps-workspace-product-lst',
-  templateUrl: './product-lst.component.html',
-  styleUrls: ['./product-lst.component.scss']
+    selector: 'ngshop-product-lst',
+    templateUrl: './product-lst.component.html',
+    styleUrls: ['./product-lst.component.scss']
 })
 export class ProductLstComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
