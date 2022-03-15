ac65e19391c81c34c75d7b35937f9a88b76e65ce
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-15 21:35:50 +0000

    change selector prefix to admin

--- apps-workspace/apps/admin/src/app/app.component.ts
+++ apps-workspace/apps/admin/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'apps-workspace-root',
+    selector: 'admin-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
