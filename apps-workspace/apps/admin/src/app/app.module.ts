53a0098faa2f19f16c8d007783125e0bc5f575e3
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-15 21:55:03 +0000

    set routing for sidebar and dashboard

--- apps-workspace/apps/admin/src/app/app.module.ts
+++ apps-workspace/apps/admin/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
 import { ShellComponent } from './shared/shell/shell.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
+const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            }
+        ]
+    }
+];
+
 @NgModule({
     declarations: [AppComponent, NxWelcomeComponent, ShellComponent, DashboardComponent, SidebarComponent],
-    imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
+    imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })],
     providers: [],
     bootstrap: [AppComponent]
 })
