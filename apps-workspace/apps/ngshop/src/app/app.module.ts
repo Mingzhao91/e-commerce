52e345d973fc9059b23fa327e5af034a8266edb3
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-15 21:22:22 +0000

    import browser animations module

--- apps-workspace/apps/ngshop/src/app/app.module.ts
+++ apps-workspace/apps/ngshop/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
@@ -13,7 +13,7 @@ import { UiModule } from '@apps-workspace/ui';
 
 @NgModule({
     declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductLstComponent, HeaderComponent, FooterComponent],
-    imports: [BrowserModule, AppRoutingModule, UiModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, UiModule],
     providers: [],
     bootstrap: [AppComponent]
 })
