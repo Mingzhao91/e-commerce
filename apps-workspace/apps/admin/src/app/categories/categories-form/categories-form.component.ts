9e453bd904f931cf8407004a1a4d034a23a94bc7
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-19 21:57:53 +0000

    adjust form to handle category edition and creation

--- apps-workspace/apps/admin/src/app/categories/categories-form/categories-form.component.ts
+++ apps-workspace/apps/admin/src/app/categories/categories-form/categories-form.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 import { timer } from 'rxjs';
 
 import { MessageService } from 'primeng/api';
 
-import { CategoriesService } from '@apps-workspace/products';
+import { CategoriesService, Category } from '@apps-workspace/products';
 
 @Component({
     selector: 'admin-categories-form',
@@ -14,20 +15,60 @@ import { CategoriesService } from '@apps-workspace/products';
     styleUrls: ['./categories-form.component.scss']
 })
 export class CategoriesFormComponent implements OnInit {
-    public form: FormGroup = new FormBuilder().group({
-        name: ['', [Validators.required]],
-        icon: ['', [Validators.required]]
-    });
+    public form: FormGroup;
     public isSubmitted = false;
+    public editMode = false;
+    public currentCategoryId: string;
 
-    constructor(private categoriesService: CategoriesService, private messageService: MessageService, private location: Location) {}
+    constructor(
+        private categoriesService: CategoriesService,
+        private messageService: MessageService,
+        private location: Location,
+        private route: ActivatedRoute
+    ) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.form = new FormBuilder().group({
+            name: ['', [Validators.required]],
+            icon: ['', [Validators.required]]
+        });
+
+        this._checkEditMode();
+    }
 
     onSubmit() {
         this.isSubmitted = true;
         if (this.form.invalid) return;
-        this.categoriesService.createCategories(this.form.value).subscribe({
+
+        if (this.editMode) {
+            this._updateCategory({ ...this.form.value, id: this.currentCategoryId });
+        } else {
+            this._addCategory(this.form.value);
+        }
+    }
+
+    private _updateCategory(category: Category) {
+        this.categoriesService.updateCategories(category).subscribe({
+            next: (response) => {
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: 'Category is updated!'
+                });
+                timer(2000).subscribe(() => this.location.back());
+            },
+            error: (error) => {
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: 'Category is not updated!'
+                });
+            }
+        });
+    }
+
+    private _addCategory(category: Category) {
+        this.categoriesService.createCategories(category).subscribe({
             next: (response) => {
                 this.messageService.add({
                     severity: 'success',
@@ -41,6 +82,21 @@ export class CategoriesFormComponent implements OnInit {
                     severity: 'error',
                     summary: 'Error',
                     detail: 'Category is not created!'
+                });
+            }
+        });
+    }
+
+    private _checkEditMode() {
+        this.route.params.subscribe((params) => {
+            if (params['id']) {
+                this.editMode = true;
+                this.currentCategoryId = params['id'];
+                this.categoriesService.getCategory(params['id']).subscribe((category) => {
+                    this.form.setValue({
+                        name: category.name,
+                        icon: category.icon
+                    });
                 });
             }
         });
