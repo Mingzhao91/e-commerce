143bb0508aa814a70f49a421c4b303b150d1c028
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-19 16:50:14 +0000

    allow user to create category data using form

--- apps-workspace/apps/admin/src/app/app.module.ts
+++ apps-workspace/apps/admin/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
@@ -9,13 +11,16 @@ import { ShellComponent } from './shared/shell/shell.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
 
 import { CardModule } from 'primeng/card';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 
-const UX_MODULES = [CardModule, ToolbarModule, ButtonModule, TableModule];
+const UX_MODULES = [CardModule, ToolbarModule, ButtonModule, TableModule, InputTextModule, ToastModule];
 
 const routes: Routes = [
     {
@@ -29,14 +34,26 @@ const routes: Routes = [
             {
                 path: 'categories',
                 component: CategoriesListComponent
+            },
+            {
+                path: 'categories/form',
+                component: CategoriesFormComponent
             }
         ]
     }
 ];
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, ShellComponent, DashboardComponent, SidebarComponent, CategoriesListComponent],
-    imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }), HttpClientModule, ...UX_MODULES],
+    declarations: [AppComponent, NxWelcomeComponent, ShellComponent, DashboardComponent, SidebarComponent, CategoriesListComponent, CategoriesFormComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ...UX_MODULES
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
---
+++ apps-workspace/apps/admin/src/app/categories/categories-form/categories-form.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
+
+import { timer } from 'rxjs';
+
+import { MessageService } from 'primeng/api';
+
+import { CategoriesService } from '@apps-workspace/products';
+
+@Component({
+    selector: 'apps-workspace-categories-form',
+    templateUrl: './categories-form.component.html',
+    styleUrls: ['./categories-form.component.scss'],
+    providers: [MessageService]
+})
+export class CategoriesFormComponent implements OnInit {
+    public form: FormGroup = new FormBuilder().group({
+        name: ['', [Validators.required]],
+        icon: ['', [Validators.required]]
+    });
+    public isSubmitted = false;
+
+    constructor(private categoriesService: CategoriesService, private messageService: MessageService, private location: Location) {}
+
+    ngOnInit(): void {}
+
+    onSubmit() {
+        this.isSubmitted = true;
+        if (this.form.invalid) return;
+        this.categoriesService.createCategories(this.form.value).subscribe({
+            next: (response) => {
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: 'Category is created!'
+                });
+                timer(2000).subscribe(() => this.location.back());
+            },
+            error: (error) => {
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: 'Category is not created!'
+                });
+            }
+        });
+    }
+}
