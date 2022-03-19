fc1c5d2d30e8038627d1475de37592c906558541
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-19 16:53:25 +0000

    remove code to unsubscribe angular http request

--- apps-workspace/apps/admin/src/app/categories/categories-list/categories-list.component.ts
+++ apps-workspace/apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -1,30 +1,20 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { CategoriesService, Category } from '@apps-workspace/products';
-import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'admin-categories-list',
     templateUrl: './categories-list.component.html',
     styleUrls: ['./categories-list.component.scss']
 })
-export class CategoriesListComponent implements OnInit, OnDestroy {
+export class CategoriesListComponent implements OnInit {
     public categories: Category[] = [];
-    private componentDestroyed$ = new Subject();
 
     constructor(private categoriesService: CategoriesService) {}
 
     ngOnInit(): void {
-        this.categoriesService
-            .getCategories()
-            .pipe(takeUntil(this.componentDestroyed$))
-            .subscribe((categories) => {
-                this.categories = categories;
-            });
-    }
-
-    ngOnDestroy() {
-        this.componentDestroyed$.next(true);
-        this.componentDestroyed$.complete();
+        this.categoriesService.getCategories().subscribe((categories) => {
+            this.categories = categories;
+        });
     }
 }
