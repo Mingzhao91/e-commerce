3751e693f2d54498fe47541ff0940c758ddb441c
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-19 15:01:55 +0000

    unsubscribe observable when component get destroyed

--- apps-workspace/apps/admin/src/app/categories/categories-list/categories-list.component.ts
+++ apps-workspace/apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { CategoriesService, Category } from '@apps-workspace/products';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'admin-categories-list',
     templateUrl: './categories-list.component.html',
     styleUrls: ['./categories-list.component.scss']
 })
-export class CategoriesListComponent implements OnInit {
+export class CategoriesListComponent implements OnInit, OnDestroy {
     public categories: Category[] = [];
+    private componentDestroyed$ = new Subject();
 
     constructor(private categoriesService: CategoriesService) {}
 
     ngOnInit(): void {
-        this.categoriesService.getCategories().subscribe((categories) => {
-            this.categories = categories;
-        });
+        this.categoriesService
+            .getCategories()
+            .pipe(takeUntil(this.componentDestroyed$))
+            .subscribe((categories) => {
+                this.categories = categories;
+            });
+    }
+
+    ngOnDestroy() {
+        this.componentDestroyed$.next(true);
+        this.componentDestroyed$.complete();
     }
 }
