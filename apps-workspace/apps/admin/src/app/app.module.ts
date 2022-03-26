3398fd6146df1b63016a297c1edd9fdcb064b316
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-26 16:25:27 +0000

    create orders list and detail components

--- apps-workspace/apps/admin/src/app/app.module.ts
+++ apps-workspace/apps/admin/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { ProductsListComponent } from './pages/products/products-list/products-l
 import { ProductsFormComponent } from './pages/products/products-form/products-form.component';
 import { UsersListComponent } from './pages/users/users-list/users-list.component';
 import { UsersFormComponent } from './pages/users/users-form/users-form.component';
+import { OrdersListComponent } from './pages/orders/orders-list/orders-list.component';
+import { OrdersDetailComponent } from './pages/orders/orders-detail/orders-detail.component';
 
 import { CardModule } from 'primeng/card';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -113,7 +115,9 @@ const routes: Routes = [
         ProductsListComponent,
         ProductsFormComponent,
         UsersListComponent,
-        UsersFormComponent
+        UsersFormComponent,
+        OrdersListComponent,
+        OrdersDetailComponent
     ],
     imports: [
         BrowserModule,
