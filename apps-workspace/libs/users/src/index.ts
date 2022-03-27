19acca7788ace9cbfa0edf76b5d937a8d8961241
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-27 12:40:42 +0100

    export auth and local-storage services

--- apps-workspace/libs/users/src/index.ts
+++ apps-workspace/libs/users/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/users.module';
 export * from './lib/services/users.service';
+export * from './lib/services/auth.service';
+export * from './lib/services/local-storage.service';
 export * from './lib/models/user';
