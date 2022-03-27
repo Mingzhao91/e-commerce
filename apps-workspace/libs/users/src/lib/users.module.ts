dc77b3ac0170b4693004d3dc0f92362d1598dda7
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-27 11:39:47 +0100

    show content in login page

--- apps-workspace/libs/users/src/lib/users.module.ts
+++ apps-workspace/libs/users/src/lib/users.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-export const usersRoutes: Route[] = [];
+import { LoginComponent } from './pages/login/login.component';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+export const usersRoutes: Route[] = [
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+];
+
+const UX_MODULES = [InputTextModule, ButtonModule];
 
 @NgModule({
-    imports: [CommonModule, RouterModule]
+    imports: [CommonModule, RouterModule.forChild(usersRoutes), FormsModule, ReactiveFormsModule, ...UX_MODULES],
+    declarations: [LoginComponent]
 })
 export class UsersModule {}
