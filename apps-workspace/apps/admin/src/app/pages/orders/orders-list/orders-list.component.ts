5334935af5e1731128b6dd5ec9fb30829219ae8f
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-27 05:55:35 +0100

    show orders in table

--- apps-workspace/apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
+++ apps-workspace/apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Order, OrdersService } from '@apps-workspace/orders';
+
+const ORDER_STATUS = {
+    Pending: {
+        label: 'Pending',
+        color: 'primary'
+    },
+    Processed: {
+        label: 'Processed',
+        color: 'warning'
+    },
+    Shipped: {
+        label: 'Shipped',
+        color: 'warning'
+    },
+    Delivered: {
+        label: 'Delivered',
+        color: 'success'
+    },
+    Failed: {
+        label: 'Failed',
+        color: 'danger'
+    }
+};
+
 @Component({
-  selector: 'admin-orders-list',
-  templateUrl: './orders-list.component.html',
-  styleUrls: ['./orders-list.component.scss']
+    selector: 'admin-orders-list',
+    templateUrl: './orders-list.component.html',
+    styleUrls: ['./orders-list.component.scss']
 })
 export class OrdersListComponent implements OnInit {
+    orders: Order[] = [];
+    orderStatus: { [key: string]: { label: string; color: string } } = ORDER_STATUS;
 
-  constructor() { }
+    constructor(private ordersService: OrdersService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this._getOrders();
+    }
 
+    showOrder(orderId: string) {}
+
+    deleteOrder(orderId: string) {}
+
+    private _getOrders() {
+        this.ordersService.getOrders().subscribe((orders) => {
+            console.log(orders);
+            this.orders = orders;
+        });
+    }
 }
