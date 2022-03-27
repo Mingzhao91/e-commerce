13cae0616b7847b96acd877da38fb73864f15f4d
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-27 13:40:03 +0100

    log user out

--- apps-workspace/apps/admin/src/app/shared/sidebar/sidebar.component.ts
+++ apps-workspace/apps/admin/src/app/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '@apps-workspace/users';
+
 @Component({
     selector: 'admin-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-    constructor() {}
+    constructor(private authService: AuthService) {}
 
     ngOnInit(): void {}
+
+    logoutUser() {
+        this.authService.logout();
+    }
 }
--- apps-workspace/libs/users/src/lib/services/auth.service.ts
+++ apps-workspace/libs/users/src/lib/services/auth.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 
 import { environment } from '@env/environment';
 
 import { User } from '../models/user';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
     providedIn: 'root'
@@ -13,9 +15,14 @@ import { User } from '../models/user';
 export class AuthService {
     apiURLUsers = `${environment.apiURL}users`;
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private localStorageService: LocalStorageService, private router: Router) {}
 
     login(email: string, password: string): Observable<User> {
         return this.http.post<User>(`${this.apiURLUsers}/login`, { email, password });
+    }
+
+    logout() {
+        this.localStorageService.removeToken();
+        this.router.navigate(['/login']);
     }
 }
