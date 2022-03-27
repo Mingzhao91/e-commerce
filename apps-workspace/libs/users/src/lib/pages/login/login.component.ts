c032a4fb4491e8eaa0144fbf520410e1d26d4e5d
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-27 12:41:27 +0100

    login user and save token in local storage

--- apps-workspace/libs/users/src/lib/pages/login/login.component.ts
+++ apps-workspace/libs/users/src/lib/pages/login/login.component.ts
@@ -1,5 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
     selector: 'users-login',
@@ -9,8 +14,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
     loginFormGroup: FormGroup;
     isSubmitted = false;
+    authError = false;
+    authMessage = '';
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(private router: Router, private formBuilder: FormBuilder, private authService: AuthService, private localStorageService: LocalStorageService) {}
 
     ngOnInit(): void {
         this._initLoginForm();
@@ -29,5 +36,22 @@ export class LoginComponent implements OnInit {
 
     onSubmit() {
         this.isSubmitted = true;
+        if (this.loginFormGroup.invalid) return;
+        this.authService.login(this.loginForm['email'].value, this.loginForm['password'].value).subscribe({
+            next: (user) => {
+                this.authError = false;
+                this.localStorageService.setToken(user.token);
+                this.router.navigate(['/']);
+            },
+            error: (error: HttpErrorResponse) => {
+                this.authError = true;
+
+                if (error.status !== 400) {
+                    this.authMessage = 'Error in the server, please try again later!';
+                } else {
+                    this.authMessage = 'Email or Password is incorrect';
+                }
+            }
+        });
     }
 }
