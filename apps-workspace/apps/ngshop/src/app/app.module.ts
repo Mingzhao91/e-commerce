51f689a70cd2ea8f9bfad1df5e8b4d507f7da90c
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-28 22:02:22 +0100

    declare nav component and import products module

--- apps-workspace/apps/ngshop/src/app/app.module.ts
+++ apps-workspace/apps/ngshop/src/app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
+import { UiModule } from '@apps-workspace/ui';
+import { ProductsModule } from '@apps-workspace/products';
+
+import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductLstComponent } from './pages/product-lst/product-lst.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { UiModule } from '@apps-workspace/ui';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductLstComponent, HeaderComponent, FooterComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, UiModule],
+    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductLstComponent, HeaderComponent, FooterComponent, NavComponent],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, UiModule, ProductsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
