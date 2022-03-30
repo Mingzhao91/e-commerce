64a7276fbf81b45ecd999f478241f044c02c5db4
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-30 20:21:54 +0100

    code refactor

--- apps-workspace/libs/products/src/lib/products.module.ts
+++ apps-workspace/libs/products/src/lib/products.module.ts
@@ -5,9 +5,12 @@ import { ProductsSearchComponent } from './components/products-search/products-s
 import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
 
 import { ButtonModule } from 'primeng/button';
-import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { CheckboxModule } from 'primeng/checkbox';
+
+const UX_MODULES = [ButtonModule, CheckboxModule];
 
 const routes: Route[] = [
     {
@@ -15,8 +18,9 @@ const routes: Route[] = [
         component: ProductsListComponent
     }
 ];
+
 @NgModule({
-    imports: [CommonModule, RouterModule.forChild(routes), ButtonModule],
+    imports: [CommonModule, RouterModule.forChild(routes), ...UX_MODULES],
     declarations: [ProductsSearchComponent, CategoriesBannerComponent, ProductItemComponent, FeaturedProductsComponent, ProductsListComponent],
     exports: [ProductsSearchComponent, CategoriesBannerComponent, ProductItemComponent, FeaturedProductsComponent, ProductsListComponent]
 })
