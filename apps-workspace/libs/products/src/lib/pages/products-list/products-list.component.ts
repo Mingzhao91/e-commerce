f2ff1f3af7ed0eda10bfc8d8161e74755ac90362
Mingzhao91 <edwardwhite7@hotmail.com>
2022-03-30 20:23:04 +0100

    display category filters and lists of products

--- apps-workspace/libs/products/src/lib/pages/products-list/products-list.component.ts
+++ apps-workspace/libs/products/src/lib/pages/products-list/products-list.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Subject, takeUntil } from 'rxjs';
+import { Category } from '../../models/category';
+
+import { Product } from '../../models/product';
+
+import { CategoriesService } from '../../services/categories.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
-  selector: 'products-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+    selector: 'products-list',
+    templateUrl: './products-list.component.html',
+    styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnDestroy {
+    public products: Product[];
+    public categories: Category[];
+    public unsubscribe$: Subject<void> = new Subject();
 
-  constructor() { }
+    constructor(private productsService: ProductsService, private categoriesService: CategoriesService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this._getProducts();
+        this._getCategories();
+    }
 
+    private _getProducts() {
+        this.productsService
+            .getProducts()
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe((products) => {
+                this.products = products;
+            });
+    }
+
+    private _getCategories() {
+        this.categoriesService
+            .getCategories()
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe((categories) => {
+                this.categories = categories;
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
 }
