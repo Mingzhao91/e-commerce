d989bc9baefd1dc9ce13fad7e5a73c55e6212d0d
Mingzhao91 <edwardwhite7@hotmail.com>
2022-04-01 21:47:23 +0100

    create cart service

---
+++ apps-workspace/libs/orders/src/lib/services/cart.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+import { Cart, CartItem } from '../models/cart';
+
+export const CART_KEY = 'cart';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CartService {
+    constructor() {}
+
+    initCartLocalStorage() {
+        const initCart = { items: [] };
+        const initCartJson = JSON.stringify(initCart);
+        localStorage.setItem(CART_KEY, initCartJson);
+    }
+
+    getCart(): Cart {
+        const cartJsonString: string = localStorage.getItem(CART_KEY);
+        const cart: Cart = JSON.parse(cartJsonString);
+        return cart;
+    }
+
+    setCartItem(cartItem: CartItem): Cart {
+        const cart = this.getCart();
+        const cartItemExist = cart.items.find((item) => item.productId === cartItem.productId);
+        if (cartItemExist) {
+            cart.items.map((item) => {
+                if (item.productId === cartItem.productId) {
+                    item.quantity = item.quantity + cartItem.quantity;
+                }
+            });
+        } else {
+            cart.items.push(cartItem);
+        }
+
+        const cartJson = JSON.stringify(cart);
+        localStorage.setItem(CART_KEY, cartJson);
+
+        return cart;
+    }
+}
