2cf4bbb95f98057f88c2a911b71f66db4920d61a
Mingzhao91 <edwardwhite7@hotmail.com>
2022-04-02 16:45:49 +0100

    show cart item detail

--- apps-workspace/libs/orders/src/lib/pages/cart-page/cart-page.component.ts
+++ apps-workspace/libs/orders/src/lib/pages/cart-page/cart-page.component.ts
@@ -1,19 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { Subject, takeUntil } from 'rxjs';
+
+import { CartItemDetailed } from '../../models/cart';
+
+import { CartService } from '../../services/cart.service';
+import { OrdersService } from '../../services/orders.service';
 
 @Component({
     selector: 'orders-cart-page',
     templateUrl: './cart-page.component.html',
     styleUrls: ['./cart-page.component.scss']
 })
-export class CartPageComponent implements OnInit {
-    constructor(private router: Router) {}
+export class CartPageComponent implements OnInit, OnDestroy {
+    public unsubscribe$: Subject<void> = new Subject();
+    public cartItemsDetailed: CartItemDetailed[] = [];
 
-    ngOnInit(): void {}
+    constructor(private router: Router, private cartService: CartService, private ordersService: OrdersService) {}
+
+    ngOnInit(): void {
+        this._getCartDetails();
+    }
+
+    private _getCartDetails() {
+        this.cartService.cart$.pipe(takeUntil(this.unsubscribe$)).subscribe((cart) => {
+            cart.items.forEach((cartItem) => {
+                console.log(cartItem);
+                this.ordersService
+                    .getProduct(cartItem.productId)
+                    .pipe(takeUntil(this.unsubscribe$))
+                    .subscribe((product) => {
+                        console.log('product: ', product);
+                        this.cartItemsDetailed.push({
+                            product,
+                            quantity: cartItem.quantity
+                        });
+                    });
+            });
+        });
+    }
 
     backToShop() {
         this.router.navigate(['/products']);
     }
 
     deleteCartItem() {}
+
+    ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
 }
--- apps-workspace/libs/orders/src/lib/services/orders.service.ts
+++ apps-workspace/libs/orders/src/lib/services/orders.service.ts
@@ -12,6 +12,7 @@ import { environment } from '@env/environment';
 })
 export class OrdersService {
     apiURLOrders = `${environment.apiURL}orders`;
+    apiURLProducts = `${environment.apiURL}products`;
 
     constructor(private http: HttpClient) {}
 
@@ -37,5 +38,8 @@ export class OrdersService {
 
     getTotalSales(): Observable<number> {
         return this.http.get<number>(`${this.apiURLOrders}/get/totalsales`).pipe(map((objectValue: any) => objectValue.totalsales));
+    }
+    getProduct(productId: string): Observable<any> {
+        return this.http.get<any>(`${this.apiURLProducts}/${productId}`);
     }
 }
