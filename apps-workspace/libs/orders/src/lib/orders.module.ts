4be4161b6bfecf29e62d1dc9361785e22933f920
Mingzhao91 <edwardwhite7@hotmail.com>
2022-04-02 22:29:43 +0100

    allow user to place an order

--- apps-workspace/libs/orders/src/lib/orders.module.ts
+++ apps-workspace/libs/orders/src/lib/orders.module.ts
@@ -1,30 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CartService } from './services/cart.service';
 
 import { CartIconComponent } from './components/cart-icon/cart-icon.component';
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
+import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
 
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
 import { InputNumberModule } from 'primeng/inputnumber';
-import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputMaskModule } from 'primeng/inputmask';
+import { DropdownModule } from 'primeng/dropdown';
 
 export const routes: Route[] = [
     {
         path: 'cart',
         component: CartPageComponent
+    },
+    {
+        path: 'checkout',
+        component: CheckoutPageComponent
     }
 ];
 
-const UX_MODULES = [BadgeModule, ButtonModule, InputNumberModule];
+const UX_MODULES = [BadgeModule, ButtonModule, InputNumberModule, InputTextModule, InputMaskModule, DropdownModule];
 
 @NgModule({
-    imports: [CommonModule, FormsModule, RouterModule.forChild(routes), ...UX_MODULES],
-    declarations: [CartIconComponent, CartPageComponent, OrderSummaryComponent],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule.forChild(routes), ...UX_MODULES],
+    declarations: [CartIconComponent, CartPageComponent, OrderSummaryComponent, CheckoutPageComponent],
     exports: [CartIconComponent, CartPageComponent, OrderSummaryComponent]
 })
 export class OrdersModule {
