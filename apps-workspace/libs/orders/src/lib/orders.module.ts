3246bb461b58d2d05e696b40ef19b81513b840f9
Mingzhao91 <edwardwhite7@hotmail.com>
2022-04-02 15:57:19 +0100

    implement cart page template

--- apps-workspace/libs/orders/src/lib/orders.module.ts
+++ apps-workspace/libs/orders/src/lib/orders.module.ts
@@ -3,18 +3,27 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 
 import { CartService } from './services/cart.service';
+
 import { CartIconComponent } from './components/cart-icon/cart-icon.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 import { BadgeModule } from 'primeng/badge';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
 
-export const ordersRoutes: Route[] = [];
+export const routes: Route[] = [
+    {
+        path: 'cart',
+        component: CartPageComponent
+    }
+];
 
-const UX_MODULES = [BadgeModule];
+const UX_MODULES = [BadgeModule, ButtonModule, InputNumberModule];
 
 @NgModule({
-    imports: [CommonModule, RouterModule, ...UX_MODULES],
-    declarations: [CartIconComponent],
-    exports: [CartIconComponent]
+    imports: [CommonModule, RouterModule.forChild(routes), ...UX_MODULES],
+    declarations: [CartIconComponent, CartPageComponent],
+    exports: [CartIconComponent, CartPageComponent]
 })
 export class OrdersModule {
     constructor(cartService: CartService) {
