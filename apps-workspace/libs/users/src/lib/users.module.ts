d078b8c6cd21f719835fe240f03cb73704ed030d
Mingzhao91 <edwardwhite7@hotmail.com>
2022-04-06 20:27:06 +0100

    create NGRX state store in users library

--- apps-workspace/libs/users/src/lib/users.module.ts
+++ apps-workspace/libs/users/src/lib/users.module.ts
@@ -7,6 +7,11 @@ import { LoginComponent } from './pages/login/login.component';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromUsers from './state/users.reducer';
+import { UsersEffects } from './state/users.effects';
+import { UsersFacade } from './state/users.facade';
 
 export const usersRoutes: Route[] = [
     {
@@ -18,7 +23,16 @@ export const usersRoutes: Route[] = [
 const UX_MODULES = [InputTextModule, ButtonModule];
 
 @NgModule({
-    imports: [CommonModule, RouterModule.forChild(usersRoutes), FormsModule, ReactiveFormsModule, ...UX_MODULES],
-    declarations: [LoginComponent]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(usersRoutes),
+        FormsModule,
+        ReactiveFormsModule,
+        ...UX_MODULES,
+        StoreModule.forFeature(fromUsers.USERS_FEATURE_KEY, fromUsers.reducer),
+        EffectsModule.forFeature([UsersEffects])
+    ],
+    declarations: [LoginComponent],
+    providers: [UsersFacade]
 })
 export class UsersModule {}
