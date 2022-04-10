401146d2125387211a97da57adf42d57c6d3c2a6
Mingzhao91 <edwardwhite7@hotmail.com>
2022-04-10 15:29:00 +0100

    use heroku domain for api url calls

--- apps-workspace/environments/environment.prod.ts
+++ apps-workspace/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiURL: 'http://localhost:3000/api/v1/',
+    apiURL: 'https://eshop-backend-ming.herokuapp.com/api/v1/',
     stripePublicKey: 'pk_test_51Km1WFJSlIoNdzpqVTjlGv1cu5noZtTLDOgEcGqqwroVbrsX0vYTn5eOXbliuQ0DdONfYvH52B1LlqiCkDUvCZv800DsI0s2SX'
 };
